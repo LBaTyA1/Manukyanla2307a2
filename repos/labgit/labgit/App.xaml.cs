fcb33ca9d865f520c31cdf0182ac3b681470e7db
LBaTyA1 <blujoker52@gmail.com>
2026-04-15 11:39:13 +0700

    Update App.xaml.cs

--- repos/labgit/labgit/App.xaml.cs
+++ repos/labgit/labgit/App.xaml.cs
@@ -1,14 +1,21 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+namespace TriangleCalculator.Models;
 
-namespace labgit
+public struct Point
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public int X { get; set; }
+    public int Y { get; set; }
+    
+    public Point(int x, int y)
     {
+        X = x;
+        Y = y;
     }
-
+    
+    public override string ToString() => $"({X}, {Y})";
+    
+    public bool Equals(Point other) => X == other.X && Y == other.Y;
+    
+    public override bool Equals(object? obj) => obj is Point other && Equals(other);
+    
+    public override int GetHashCode() => HashCode.Combine(X, Y);
 }
